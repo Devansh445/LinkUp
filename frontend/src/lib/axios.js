fa2004ebddf4fb5b57ac531665196b9802e134ee
Devansh singh chahar <131586465+Devansh445@users.noreply.github.com>
2025-07-12 20:15:21 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : import.meta.env.VITE_API_URL + "/api",
+      : import.meta.env.VITE_API_URL + "api",
   withCredentials: true,
 });
 
