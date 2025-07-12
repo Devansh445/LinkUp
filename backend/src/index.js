c358dedc3a2e6f21491a2a529aea0a0742499649
Devansh singh chahar <131586465+Devansh445@users.noreply.github.com>
2025-07-12 19:25:22 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// ✅ Fix: Increase body parser size limit
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
